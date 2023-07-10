067d08e5917f022ca4959999ef979f807646e356
Brendom Gonçalves <brendom.vi@gmail.com>
2023-07-10 14:47:12 -0300

    Atualizando metodo de entrada

--- PYTHON/BEE1057.py
+++ PYTHON/BEE1057.py
@@ -1,18 +1,6 @@
-T = 1
-for _ in range(0, T):
-    N, A, B, C, Sala = 3, [], [], [], []
-    for i in range(0, N):
-        linha = input()
-        list_linha = []
-        for j, carac in enumerate(linha):
-            list_linha.append(carac)
-            if carac == 'A':
-                A.append(i)
-                A.append(j)
-            elif carac == 'B':
-                B.append(i)
-                B.append(j)
-            elif carac == 'C':
-                C.append(i)
-                C.append(j)
-        Sala.append(list_linha)    
+T = int(input())
+for caso in range(1, T + 1):
+    N = int(input())
+    mapa = [list(input().strip()) for _ in range(N)]
+    resultado = busca(mapa, N)
+    print("Case " + str(caso) + ": " + str(resultado))
